108ce778e2882d47c0be96c16f163aaa967d9c13
giselaDandrea <gidandreaok@gmail.com>
2023-07-09 16:24:25 -0300

    Actividades clase 7

---
+++ Clase_7/Ejercicio1.java
@@ -0,0 +1,53 @@
+package Clase_7;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        //Ejercicio1
+        //Craer un arreglo que guarde 5 numeros y los muestre
+        int[] numeros = new int[5]; //array
+        Scanner teclado = new Scanner(System.in); //Crea la instacia para el ingreso del usuario
+
+
+        int contador = 0;
+        while (contador < numeros.length) {
+            System.out.println("Por favor ingrese un número");
+            int numero = teclado.nextInt();
+            numeros[contador] = numero;
+            contador++;
+
+        }
+
+        contador = 0;
+        while (contador < numeros.length) {
+            System.out.println(numeros[contador]);
+            contador++;
+
+        }
+
+        //Ejercicio2 : Agregar que muestre el numero mayor y menor
+        int mayor = 0;
+        int menor = 0;
+
+        for ( contador = 0; contador < numeros.length; contador++) {
+            int num = numeros[contador];
+            if (num >= mayor) {
+                mayor = num;
+            }
+            if (num <= menor) {
+                menor = num;
+            }
+        }
+        System.out.println("El numero mayor es: " + mayor);
+        System.out.println("El numero menor es: " + menor);
+    }
+}
+
+
+
+
+
+
+
